69ef22b74d09dfe5b76b45a8c778c0c97f4af89a
Pedro Marchante <pmarc560@gmail.com>
2019-08-05 20:59:49 -0400

    completed a redux state change react project

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,14 +1,20 @@
+import { combineReducers } from "redux";
 const songsReducer = () => {
   return [
-    { title: 'Old town road', duration: '3:45' },
-    { title: 'Bad bad bad', duration: '4:02' },
-    { title: 'All star', duration: '3:03' }
+    { title: "Old town road", duration: "3:45" },
+    { title: "Bad bad bad", duration: "4:02" },
+    { title: "All star", duration: "3:03" }
   ];
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === 'SONG_SELECTED') {
+  if (action.type === "SONG_SELECTED") {
     return action.payload;
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
